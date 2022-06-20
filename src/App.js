37642da908ea6023a042b13dbd28decffd7b6be9
Danilo Gomes <danilo.dsgomes@gmail.com>
2022-06-20 11:47:30 -0300

    ajeitado resposividade

--- src/App.js
+++ src/App.js
@@ -12,15 +12,24 @@ function App() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d5c57be0858cd6cd02494d79769700b8&units=metric&lang=pt_br`
     Axios.get(url)
       .then((resp) => {
-        setWeather(resp.data)
+        setWeather({
+          name: resp.data.name,
+          description: resp.data.weather[0].description,
+          icon: resp.data.weather[0].icon,
+          temp: resp.data.main.temp.toFixed(),
+          min: resp.data.main.temp_min.toFixed(),
+          max: resp.data.main.temp_max.toFixed(),
+          feels: resp.data.main.feels_like.toFixed()
+        })
       })
     setCityName('')
   }
 
+  const now = new Date()
+
   const date = () => {
     const dayName = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
     const monName = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Agosto", "Outubro", "Novembro", "Dezembro"]
-    const now = new Date()
     return (
       <div>
         <p className="Date">
@@ -33,17 +42,32 @@ function App() {
 
   const weatherBackground = () => {
  
-
-    if(weather.weather[0].icon === "10d") return "Rain"
-
-
+    if(weather.icon == null && (now.getHours() >= 5 && now.getHours() < 18)) return "Day"
+    if(weather.icon == null && (now.getHours() >= 18 && now.getHours() < 5)) return "Night"
+    if(weather.icon === "01d") return "ClearSky"
+    if(weather.icon === "01n") return "ClearSkyN"
+    if(weather.icon === "02d") return "FewClouds"
+    if(weather.icon === "02n") return "FewCloudsN"
+    if(weather.icon === "03d") return "ScatteredClouds"
+    if(weather.icon === "03n") return "ScatteredCloudsN"
+    if(weather.icon === "04d") return "BrokenClouds"
+    if(weather.icon === "04n") return "BrokenCloudsN"
+    if(weather.icon === "09d" || weather.icon === "10d") return "Rain"
+    if(weather.icon === "09n" || weather.icon === "10n") return "RainN"
+    if(weather.icon === "11d") return "Thunderstorm"
+    if(weather.icon === "11n") return "ThunderstormN"
+    if(weather.icon === "13d") return "Snow"
+    if(weather.icon === "13n") return "SnowN"
+    if(weather.icon === "50d") return "Mist"
+    if(weather.icon === "50n") return "MistN"
+    
     return ""
   }
 
   return (
     <div className="App">
 
-      <header>
+      <header className={weatherBackground()}>
         <h1>
           Find Weather Forecast
         </h1>
@@ -66,46 +90,45 @@ function App() {
 
         <div className='Container'>
 
-          {weather.weather ?
-            <>
+          {weather.name ?
+            <div>
               <div className="Location">
                 <p>{weather.name}</p>
                 <MapPin size="30" />
               </div>
 
-              <p>{weather.weather[0].description}</p>
-            </>
+              <p>{weather.description}</p>
+            </div>
             : null}
 
           <div className="Info">
 
             {date()}
 
-            {weather.weather ?
+            {weather.name ?
               <>
-                <p>{weather.main.temp.toFixed()}º</p>
-                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+                <p>{weather.temp}º</p>
+                <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} />
               </>
               : null}
 
           </div>
 
-          {weather.weather ?
+          {weather.name ?
 
             <div className="MoreInfo">
               <div>
-                <p>{weather.main.temp_min.toFixed()}º</p>
+                <p>{weather.min}º</p>
                 <p>MIN</p>
               </div>
               
-
               <div>
-                <p>{weather.main.temp_max.toFixed()}º</p>
+                <p>{weather.max}º</p>
                 <p>MAX</p>
               </div>
 
               <div>
-                <p>{weather.main.feels_like.toFixed()}º</p>
+                <p>{weather.feels}º</p>
                 <p>FEELS</p>
               </div>
             </div>
